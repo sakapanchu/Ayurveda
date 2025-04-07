8f5eba6a006240ea578e8a18e9e0d5deda02f0a8
sakapanchu <sakaaniya2000@gmail.com>
2025-04-07 14:57:39 +0800

    netlify file upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://ayurveda-hayd.onrender.com",
+      "/uploads/": "https://ayurveda-hayd.onrender.com",
     },
   },
 });
